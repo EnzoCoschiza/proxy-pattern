62b07dff95333ec9d89b792decf08433121c9084
EnzoCoschiza <enzocoschiza15@gmail.com>
2025-10-27 01:06:17 -0300

    changes in the video of "Juego"

--- src/App.tsx
+++ src/App.tsx
@@ -146,7 +146,7 @@ function App() {
                       playsInline
                       preload="metadata"
                     >
-                      <source src="/video-agente.mp4" type="video/mp4" />
+                      <source src="/video-agente2.mp4" type="video/mp4" />
                       Tu navegador no soporta el elemento de video.
                     </video>
                   </div>
